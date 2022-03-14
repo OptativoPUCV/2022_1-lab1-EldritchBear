07db5d14ecf42b4fa670e1192f40b896c57ea65e
EldritchBear <>
2022-03-14 01:17:31 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -125,10 +125,10 @@ void sumaV2(int a1, int a2, int b1, int b2, Vector *c){
   int i;
 
   for (i = 0; i < capacidad; i++){
-    c->datos[i]+=(a1+b1);
+    c=(a1+b1);
   }
 
   for (i = 0; i < capacidad; i++){
-    c->datos[i]+=(a2+b2);
+    c+=(a2+b2);
   }
 }
