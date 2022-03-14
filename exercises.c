176d30e26362a06a59a8702ce93d8b02ab5a345a
EldritchBear <>
2022-03-14 00:26:49 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -89,7 +89,6 @@ Programe la función void asignarValor(Vector * v, int i, int valor),
 la cual asigna el valor a la posición i del vector v.
 */
 void asignarValor(Vector * v, int i, int valor) {
-  int i;
   Vector *sandia = (Vector*) malloc (sizeof(Vector*));
   for (i = 0 ; i < sandia->capacidad ; i++){
     sandia->datos[i] = valor;
